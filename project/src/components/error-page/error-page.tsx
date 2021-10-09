d7a12d53a31bf455dcb6c3534c69e9125d400112
Анна <AnnaMandrik1984@yandx.ru>
2021-10-09 23:47:36 +0300

    Сделана маршрутизация. Хэдер вынесен отдельно. Сделана фаворитная карточка

--- project/src/components/error-page/error-page.tsx
+++ project/src/components/error-page/error-page.tsx
@@ -1,13 +1,15 @@
+import {Link} from 'react-router-dom';
+import {AppRoute} from '../../const';
+
 function ErrorPage(): JSX.Element {
   return (
 
     <main className="page__main page__main--favorites page__main--favorites-empty">
       <div className="page__favorites-container container">
         <section className="favorites favorites--empty">
-          <h1 className="visually-hidden">Favorites (empty)</h1>
           <div className="favorites__status-wrapper">
             <b className="favorites__status">404. Page not found</b>
-            <p className="favorites__status-description">Please, click to go to main page</p>
+            <Link className="favorites__status-description" to={AppRoute.Main}>Return to the main page</Link>
           </div>
         </section>
       </div>
